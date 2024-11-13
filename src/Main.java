7f56bc94ab768b7211b1b15997155913a4488690
Miguel Angel Rojas <miguelonirvanito02@gmail.com>
2024-11-12 23:23:18 -0500

    metodos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Parqueadero par1 = new Parqueadero();
+        String[] parqueaderos = {"a01","a02","a03","a04","b01","b02","b03"};
+        par1.setParquins(parqueaderos);
+        par1.agregarRegistro();
     }
 }
--- src/Parqueadero.java
+++ src/Parqueadero.java
@@ -1,13 +1,18 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class Parqueadero {
 
     private String nombre;
+    //  key,value
+    //{[a02,Registro:{Vehiculo,Usuario,horaEntrada,horaSalida}]}
     private HashMap<String,Registro> vehiculos;
     private int cantidadEspaciosTotal;
     private List<Registro> historialTotalVehiculos;
+    //[a01-a02-a03-a04-b01-b02-b03]
     private String[] parquins;
 
 
@@ -61,19 +66,41 @@ public class Parqueadero {
                 ", vehiculos=" + vehiculos +
                 ", cantidadEspaciosTotal=" + cantidadEspaciosTotal +
                 ", historialTotalVehiculos=" + historialTotalVehiculos +
+                ", parquins=" + Arrays.toString(parquins) +
                 '}';
     }
 
-    public void mostrarHistorial(LocalDateTime fechaHoraInicioFiltro,LocalDateTime fechaHoraFinFiltro){
-
+    public void mostrarHistorial(LocalDateTime fechaHoraInicioFiltro, LocalDateTime fechaHoraFinFiltro){
+        for (Registro historialTotalVehiculo : historialTotalVehiculos) {
+            System.out.println(historialTotalVehiculo);
+        }
     }
-    public void mostrarEspaciosLibres(){
-
+    public List<String> mostrarEspaciosLibres(){
+        List<String> parqueaderosLibres = new ArrayList<>();
+        if (vehiculos.isEmpty()){
+            return Arrays.stream(parquins).toList();
+        }
+        if(vehiculos.size()==parquins.length) {
+            System.out.println("No hay espacios disponibles");
+        }else{
+            for (String parqueadero : parquins) {
+                if (!vehiculos.containsKey(parqueadero)) {
+                    System.out.println(parqueadero);
+                    parqueaderosLibres.add(parqueadero);
+                }
+            }
+        }
+        return parqueaderosLibres;
     }
     public void mostrarVehiculosActivosParquedero(){
-
+        System.out.println("los vehiculos actualmente que se encuentran en el parqueadero:");
+        for (String parqueadero : vehiculos.keySet()){
+            System.out.println(vehiculos.get(parqueadero).getHoraEntrada()+" - "+vehiculos.get(parqueadero).getVehiculo());
+        }
     }
-    public void agregarRegistro(Registro registro){
+    public void agregarRegistro(){
+        System.out.println("Escribe el nombre del parqueadero en el que vas a estacionar:");
+        List<String> parqueaderos = this.mostrarEspaciosLibres();
 
     }
     public void cerrarRegistro(String Parquin){
